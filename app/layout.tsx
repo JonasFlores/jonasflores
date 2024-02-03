e90f8a83a66ac8cf79ca18a5fb0b12cf84fe7414
Jonas Flores <jonassilvaflores@hotmail.com>
2024-02-03 23:18:31 +1300

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,39 +16,29 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/* <body>
-          <div className='grid grid-cols-1 lg:grid-cols-2 grid-rows-[3.5rem_auto] h-screen lg:grid-rows-none  '>
-            <div className=''>sidebar</div>
-            <div className='overflow-y-auto'>
-            </div>
-          </div> 
-      </body> */}
+<html lang="en">
+<body className="bg-gray-950 bg-[url('/grid.svg')] overflow-hidden">
 
-      
-      <body className="bg-gray-950 bg-[url('/grid.svg')] overflow-hidden">
+  {/* wrapper */}
+  <div className='grid grid-cols-1 lg:grid-cols-12 grid-rows-[3.5rem_auto] lg:grid-rows-none h-screen'>
+    
+    <aside className='lg:col-span-3'>
+      <NavBar/>
+    </aside>
 
-      {/* wrapper */}
-      <div className='grid grid-cols-1 lg:grid-cols-12 grid-rows-[3.5rem_auto] lg:grid-rows-none h-screen'>
-        
-          <aside className='lg:col-span-3 '>
-            <NavBar/>
-          </aside>
-
-          <main className="lg:col-span-9 overflow-y-auto touch-auto ">
-          <div className="mx-auto space-y-8 px-2 m-3">
-            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
-              <div className="rounded-lg p-3.5 bg-white">
-                {children}
-                </div>
-            </div>
+    <main className="lg:col-span-9 overflow-y-auto touch-auto ">
+      <div className="mx-auto space-y-8 px-2 m-3">
+        <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
+          <div className="rounded-lg p-3.5 bg-white">
+            {children}
           </div>
-          </main>
-
+        </div>
       </div>
+    </main>
 
-      </body>
-    </html>
+  </div>
+</body>
+</html>
   )
 }
 // https://tailwindcss.com/docs/responsive-design sm / md / lg / xl / 2xl
