c119afd6458bb2510d967091e3d70ff4b91f62d8
Jonas Flores <jonassilvaflores@hotmail.com>
2024-03-03 12:00:04 +1300

    Refactored converter-tool Page

--- app/converter-tool/page.tsx
+++ app/converter-tool/page.tsx
@@ -1,139 +1,94 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { SyntheticEvent, useEffect, useState } from "react"
 
 export default function Page(){
-  const [txt, setTxt] = useState('')
-  const [json, setJson] = useState('')
-  const [array, setArray] = useState([['']])
-  const regEx = /[ ]/mg
+  
+  const [editableTxt, setEditableTxt] = useState('')
+  const [generatedCSV, setGeneratedCSV] = useState('')
+  
+  //on_EditableTxtInput_Change
+  function onChangeEditableTxt(event: SyntheticEvent & {target : HTMLTextAreaElement}){
+    setEditableTxt(event.target.value)
+  }
+  
+  //EditableTXT_Changes_Listener
+  useEffect(()=>{ setGeneratedCSV(CsvGeneratorRealTime(editableTxt)) }, [editableTxt])
+  
 
-  // useEffect(()=>{
-  //   let transform = txt.replace(regEx, ';') 
+  function CsvGeneratorRealTime(text: string): string{
+    
+    const regExSpaces = /[ ]/mg
+    const regExNewLines = /[\r\n]/mg
+    
+    text = text.replace(regExSpaces, ';')
+    text = text.replace(regExNewLines, ';\r\n')
 
-  //   setJson(transform)
-  // }, [txt])
+    return text
+  
+  }
 
-  function run(){
-    let convertedJson = arrayToJson(textToArray(txt),[])
-    console.log(convertedJson)
-    setJson('')
+  function CheckGeneratedCSV(csv: string): { isValid: boolean, errorMessage: string }{
+    let isValid = false
+    let errorMessage = ''
+
+    if(csv.length == 0){
+      isValid = false
+      errorMessage = 'The CSV is empty'
+    }else{
+      errorMessage = 'no implemented'
+    }
+    
+    return {isValid: isValid, errorMessage: errorMessage}
+  }
+
+  function FixGeneratedCSV_OnBlur(): void {
+    
+    let { isValid, errorMessage } = CheckGeneratedCSV(generatedCSV)
+
+    console.log(errorMessage)
+    
+    if(!isValid){
+      setGeneratedCSV(generatedCSV + ';')
+    }
+    
   }
 
   return (
     <>
-      <div className='ml-10'>
+      <div className='mx-10 text-slate-800'>
         <div className='font-semibold'>TXT to JSON Converter</div>
         <div className='text-sm'>
-        Using js functions from: <a href="https://github.com/fraigo/text-to-json" className="text-blue-500 font-medium"> Github/fraigo</a> 
+          Check the code to see how I develop the tool using: js functions and events.
         </div>
+        <hr className='my-4'/>
       </div>
-      <hr className='my-4'/>
 
-      
-      <textarea name="txt" id="inputTxt" value={txt} onChange={e => setTxt(e.target.value)} placeholder="Insert your text here" rows={10}
-        className="w-full overflow-y-auto p-3 border shadow-sm border-slate-500 focus:outline-none focus:border-green-500 focus:ring-green-500 rounded-md text-xs focus:ring-1"
-      ></textarea>
-      {/* <div className="select-all text-xs whitespace-pre-wrap border border-slate-500 rounded-md p-3 mt-2 min-h-40">
-        {json}
-      </div> */}
+      <div className="mt-2 px-2 ">
+        <h3 className="font-medium px-2 text-slate-600">Editable TXT</h3>
+
+        <textarea name="txt" id="inputTxt" 
+          value={editableTxt} 
+          onChange={(event: SyntheticEvent & {target : HTMLTextAreaElement}) => onChangeEditableTxt(event)} 
+          onBlur={(event: SyntheticEvent) => FixGeneratedCSV_OnBlur()} 
+          placeholder="Insert your text here"
+          rows={5}
+          className="w-full overflow-y-auto p-2 border shadow-lg border-slate-500 focus:outline-none rounded-md"
+        ></textarea>
+      </div>
+
+      <div className="mt-2 px-2">
+        <h3 className="font-medium px-2 text-slate-600">Result</h3>
+        <div className="whitespace-pre-wrap select-all border border-slate-500 rounded-md p-2 min-h-28 shadow-lg ">
+          {generatedCSV}
+        </div>
+      </div>      
       
       <div className="grid justify-items-end">
-        <button onClick={run} className='rounded bg-yellow-600 text-white py-4 px-8 mt-5' >Run</button>
+        <button className='rounded bg-yellow-600 text-white py-4 px-8 mt-5' >Run</button>
       </div>
-      <div className="text-xs whitespace-pre-wrap border border-slate-500 rounded-md p-3 mt-2 min-h-40">
-        {json}
-      </div>
+        
     </>
   )
 
-}
-
-function getSeparator(line: string){
-  
-  let sep=[
-    {sep:",",cols:line.split(",")},
-    {sep:";",cols:line.split(";")},
-    {sep:"\t",cols:line.split("\t")},
-    {sep:"|",cols:line.split("|")},
-  ]
-
-  sep.sort(function(a,b){ 
-    if (b.cols.length>a.cols.length) return 1;
-    if (b.cols.length<a.cols.length) return -1;
-    return 0;
-  });
-
-  return sep[0].sep; 
-}
-
-
-function textToArray(string: string ){
-	
-  let lines = string.split("\n");
-  let sep = getSeparator(lines[0]);
-  let result = [];
-    
-  for(let index in lines){
-    lines[index] = lines[index].trim();
-    let cols = lines[index].split(sep);
-
-    console.log(cols)
-    if(cols.length > 0){
-      
-      if(cols[0] == ""){
-        console.log('aqui')
-        for(var col in cols){
-          if(cols[col] != ""){
-            result[result.length-1][col]+="\n"+cols[col];
-          }
-        }
-      }else{
-        result.push(cols);
-      }
-    }
-  }
-  
-  return result;
-}
-
-function arrayToJson(array:any , header:any){
-  
-  let tmp = array.concat([]);
-
-  if (!header){
-    header = tmp.shift();
-  }
-
-  let result = [];
-  for(let index in tmp){
-    let row:any = {};
-    for(let col in header){
-      let colname=header[col];
-      let colvalue=tmp[index][col];
-      
-      for(let key in header){
-        let header1=header[key];
-        if (colvalue){
-          colvalue=colvalue.replace("${"+header1+"}",tmp[index][key]);
-        }
-                
-            }
-      if (colvalue=='#row'){
-        colvalue = parseInt(index)*1;
-      }
-      
-      if (colvalue && colvalue.length && colvalue.indexOf('#row+')==0){
-        let parts=colvalue.split('+');
-        colvalue=parseInt(index)*1+parts[1]*1;
-      }
-      if (colname && colname.indexOf('#')==0){
-        colvalue*=1;
-        colname=colname.substring(1);
-      }
-      row[colname]=colvalue;
-    }
-    result.push(row);
-  }
-  return result;
 }
