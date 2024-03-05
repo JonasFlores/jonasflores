6db0ac7e7a2a2ece4b0952f853d6d5f198e86caa
Jonas Flores <jonassilvaflores@hotmail.com>
2024-03-05 21:23:07 +1300

    Component wage-tax-nz is consuming data from json

--- app/wage-tax-nz/page.tsx
+++ app/wage-tax-nz/page.tsx
@@ -1,50 +1,52 @@
+'use client'
+
 import clsx from 'clsx';
 import { TableFilter } from './components';
-import { tableIrdPaye, tableIrdPayeTest } from '@/data/table_IRD_PAYE';
-
-
-function txtToArray(txt: string){
-  let data = txt.split('\n')
-  let titles = data[0].split(' ')
-  return {data, titles}
-}
+import { tableIrdPaye } from '@/data/table_IRD_PAYE';
+import { useEffect, useState } from 'react';
 
 export default function Page(){
   
-  let {data, titles} = txtToArray(tableIrdPayeTest)
+  const [search, setSearch] = useState<string>('')
+  const [table_PaginationIndex, setTable_PaginationIndex] = useState<number>(0)
+  const [table_RowsToPage, setTable_RowsToPage] = useState<number>(10)
+
+  function changePagination(isForward:boolean): void{
+    if(!isForward && table_PaginationIndex == 0){ return }
+    isForward ? setTable_PaginationIndex(table_PaginationIndex + table_RowsToPage) : setTable_PaginationIndex(table_PaginationIndex - table_RowsToPage)
+  }
 
   return(
     <>
-    <TableFilter />
+    <TableFilter 
+      search={search}
+      setSearch={setSearch}
+    />
     <table className='table-auto border-collapse border border-slate-400 w-full text-center'>
       <caption className="caption-top border-t border-x border-slate-300 py-4 bg-slate-100 text-slate-500 font-medium">
         Weekly and fortnightly PAYE deduction tables | IR340 | September 2023 
       </caption>
       <thead>
         <tr className='bg-slate-200 text-slate-700'>
-          <th className='border border-slate-300 p-1'>{titles[0]}</th>
-          <th className='border border-slate-300 p-1'>{titles[1]}</th>
-          {/* <th className='border border-slate-300 p-1'>{titles[2]}</th> */}
+          <th className='border border-slate-300 p-1'>{Object.keys(tableIrdPaye[0])[0]}</th>
+          <th className='border border-slate-300 p-1'>{Object.keys(tableIrdPaye[0])[1]}</th>
           <th className='border border-slate-300 p-1'>Net Pay</th>
         </tr>
       </thead>
       <tbody>
         
-        {data.map((line, index)=> {
-          const cols = line.split(' ')
-          let col1 = parseFloat(cols[0])
-          let col2 = parseFloat(cols[1])
-          let col3 = cols[2]
+        {tableIrdPaye.slice(table_PaginationIndex,table_PaginationIndex + table_RowsToPage).map((line, index)=> {
+          let col1 = parseFloat(line.gross_pay)
+          let col2 = parseFloat(line.M)
           let netPay = col1 - col2
 
           if(index == 0){return}
           
           return (
             
-            <tr key={line} className={clsx({"bg-slate-100" : index % 2 == 0})}>
+            <tr key={index} className={clsx({"bg-slate-100" : index % 2 == 0})}>
                 <td className='border border-slate-300 p-1'>{col1}.00</td>
                 <td className='border border-slate-300 p-1'>{col2}</td>
-                {/* <td className='border border-slate-300 p-1'>{col3}</td> */}
                 <td className='border border-slate-300 p-1'>{netPay.toFixed(2)}</td>
             </tr>
           
@@ -52,6 +54,15 @@ export default function Page(){
       })}
       </tbody>
     </table>
+      <div className='bg-slate-300 w-full flex flex-row justify-end mt-2 text-center p-2 cursor-pointer rounded-lg select-none'>
+        <div className='basis-1/3 hover:font-semibold'
+          onClick={() => changePagination(false)}
+          >Back</div>
+        <div className='basis-1/3 text-xs p-1 text-slate-600'>{table_PaginationIndex} - {table_PaginationIndex+table_RowsToPage}</div>
+        <div className='basis-1/3 hover:font-semibold'
+          onClick={() => changePagination(true)}
+          >Forward</div>
+      </div>
 
 
     
