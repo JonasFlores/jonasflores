67371d44dc7b45cd8a59b1bc50390897f977a778
Jonas Flores <jonassilvaflores@hotmail.com>
2024-03-31 13:11:42 +1300

    Craating the table and pagination ui components

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,55 +1,10 @@
-import Header from "../components/curriculum-vitae/header"
+import Table from "../components/ui/table/table";
 
 export default function Page(){
   return(
     <>
-      <TestTypography header="text by props">
-        <h1 className="font-bold text-4xl">Jonas Flores</h1>
-        <h2 className="font-bold text-xl sm:text-2xl md:text-3xl">Word h2</h2>
-        <h3 className="font-bold text-lg sm:text-xl md:text-2xl">Word h3</h3>
-        <h4 className="font-semibold text-md sm:text-lg md:text-xl">Word h4</h4>
-        <h5>Word h5</h5>
-        <h6>Word h6</h6>
-        <p>Paragraph</p>
-        <a href="">Link</a>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis repudiandae deserunt dolorem ex voluptatum voluptatibus, adipisci ratione labore, sit officiis corrupti iure eveniet explicabo consectetur itaque obcaecati vero nostrum unde.</p>
-      </TestTypography>
+      <Table></Table>      
     </>
   )
 }
 
-function TestHeaderCV(){
-  return(
-  <>
-    <Header></Header>
-  </>
-  )
-}
-
-function TestTypography({children, header}:{children: React.ReactNode, header?: string}){
-  return(<>
-
-    <div className="">
-      {children}
-
-    </div>  
-    </>)
-}
-
-function TestFlexBox(){
-  return(
-    <>
-    <h1 className="leading-normal text-center">Testing Flex Box with TailWindCSS</h1>
-    <br />
-      <div className="flex flex-row">
-        <div className="basis-1/4 hover:basis-1/2 bg-purple-400">
-          1
-        </div>
-        <div className="basis-1/2 bg-purple-900">
-          2
-        </div>
-        
-      </div>
-    </>
-  )
-}
--- app/wage-tax-nz/page.tsx
+++ app/wage-tax-nz/page.tsx
@@ -3,45 +3,44 @@
 import clsx from 'clsx';
 import { TableFilter } from './components';
 import { tableIrdPaye } from '@/data/table_IRD_PAYE';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import Pagination from '../components/ui/table/pagination';
 
 export default function Page(){
   
   const [search, setSearch] = useState<string>('')
-  const [table_PaginationIndex, setTable_PaginationIndex] = useState<number>(0)
-  const [table_RowsToPage, setTable_RowsToPage] = useState<number>(10)
+  const [page, setPage] = useState<number>(1)
+  const [rowsPerPage, setRowsPerPage] = useState<number>(150)
+  const tableRecords = () => tableIrdPaye
 
-  function changePagination(isForward:boolean): void{
-    if(!isForward && table_PaginationIndex == 0){ return }
-    isForward ? setTable_PaginationIndex(table_PaginationIndex + table_RowsToPage) : setTable_PaginationIndex(table_PaginationIndex - table_RowsToPage)
-  }
 
   return(
     <>
-    <TableFilter 
+    {/* <TableFilter 
       search={search}
       setSearch={setSearch}
-    />
-    <table className='table-auto border-collapse border border-slate-400 w-full text-center'>
+    /> */}
+    <Pagination page={page} rowsPerPage={rowsPerPage} total={tableRecords().length} setPage={setPage}>
+    </Pagination>
+    <table className='mt-3 table-auto border-collapse border border-slate-400 w-full text-center'>
       <caption className="caption-top border-t border-x border-slate-300 py-4 bg-slate-100 text-slate-500 font-medium">
         Weekly and fortnightly PAYE deduction tables | IR340 | September 2023 
       </caption>
       <thead>
         <tr className='bg-slate-200 text-slate-700'>
-          <th className='border border-slate-300 p-1'>{Object.keys(tableIrdPaye[0])[0]}</th>
-          <th className='border border-slate-300 p-1'>{Object.keys(tableIrdPaye[0])[1]}</th>
+          <th className='border border-slate-300 p-1'>{Object.keys(tableRecords()[0])[0]}</th>
+          <th className='border border-slate-300 p-1'>{Object.keys(tableRecords()[0])[1]}</th>
           <th className='border border-slate-300 p-1'>Net Pay</th>
         </tr>
       </thead>
       <tbody>
         
-        {tableIrdPaye.slice(table_PaginationIndex,table_PaginationIndex + table_RowsToPage).map((line, index)=> {
+        {tableRecords().slice((page-1)*rowsPerPage, rowsPerPage*page).map((line, index)=> {
+          
           let col1 = parseFloat(line.gross_pay)
-          let col2 = parseFloat(line.M)
+          let col2 = parseFloat(line.M?line.M:"0")
           let netPay = col1 - col2
 
-          if(index == 0){return}
-          
           return (
             
             <tr key={index} className={clsx({"bg-slate-100" : index % 2 == 0})}>
@@ -53,19 +52,7 @@ export default function Page(){
           )
       })}
       </tbody>
-    </table>
-      <div className='bg-slate-300 w-full flex flex-row justify-end mt-2 text-center p-2 cursor-pointer rounded-lg select-none'>
-        <div className='basis-1/3 hover:font-semibold'
-          onClick={() => changePagination(false)}
-          >Back</div>
-        <div className='basis-1/3 text-xs p-1 text-slate-600'>{table_PaginationIndex} - {table_PaginationIndex+table_RowsToPage}</div>
-        <div className='basis-1/3 hover:font-semibold'
-          onClick={() => changePagination(true)}
-          >Forward</div>
-      </div>
-
-
-    
+    </table> 
     </>
   )
 }
